d3135d45ef912914bfa10b063d53d05c15163ae6
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-11-09 12:23:37 -0800

    hi

--- instructions.py
+++ instructions.py
@@ -6,18 +6,18 @@
     # Modified:
 
 
-#def instructions():
-   # while True:
-instructions = input("Do you want the instructions? yes or no: ").lower().replace(" ", "")
-print (instructions)
-        #if instructions == "no":
-            #print ("Alright, lets go!")
-           # break 
-        #elif instructions== "yes":
-           # print ("insert instructions")
-            #break 
-      #  else: 
-           # print ("Invalid input. Please enter yes or no")
-            
-#instructions()
-            
+def instructions():
+   while True:
+      instructions = input("Do you want the instructions? yes or no: ").lower().replace(" ", "")
+      
+      if instructions == "no":
+         print ("Alright, lets go!")
+         break 
+      elif instructions== "yes":
+         print ("insert instructions")
+         break 
+      else: 
+        # print ("Invalid input. Please enter yes or no")
+                  
+      #instructions()
+                  
