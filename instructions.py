97e1cca1961deb5c8afbc6524cad8449f7b789d0
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2023-11-11 16:52:51 -0800

    moreeeeeeee

--- instructions.py
+++ instructions.py
@@ -1,18 +1,22 @@
 #!python3
+    # Will display instructions
+    # input parameters: none needed
+    # output parameters: None
+    # Author:
+    # Modified:
+
 
 def instructions():
-   while True:
-      instructions = input("Do you want the instructions? yes or no: ").lower().replace(" ", "")
-      
-      if instructions == "no":
-         print ("Alright, lets go!")
-         break 
-      elif instructions== "yes":
-         print ("These are the instructions! The program will ask you what you want to calculate\nThen, it will ask you the inputs needed.\nLet's Go! ")
-         break 
-      else: 
-         print ("Invalid input. Please enter yes or no")
-                 
+    while True:
+        instructions = input("Do you want the instructions? yes or no: ").lower().replace(" ", "")
+        if instructions == "no":
+            print ("Alright, lets go!")
+            break 
+        elif instructions== "yes":
+            print ("insert instructions")
+            break 
+        else: 
+            print ("Invalid input. Please enter yes or no")
+            
 instructions()
-                  
-
+            
