51ffd4228bd2f104a061b177d39501c8492a1912
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2023-11-15 13:34:42 -0800

    trying coliur

--- assignment.py
+++ assignment.py
@@ -26,8 +26,8 @@ def instructions():
 def start(): #Player chooses which function to execute
     while True:
         Ins='\033[1;31;40m Type a number to commence the specific function.\n:'
-        VC='\033[1;31;40m  \n  Volume Calculations:\n           Sphere==>1\n             Cube==>2\nRectangular Prism==>3\n             Cone==>4'
-        SAC='\033[1;31;40m  \n\nSurface Area Calculations:\n               Sphere==>5\n                 Cube==>6\n    Rectangular Prism==>7\n                 Cone==>8'
+        VC='\033[1;32;40m Volume Calculations: \n           Sphere==>1\n             Cube==>2\nRectangular Prism==>3\n             Cone==>4'
+        SAC='\033[1;32;40m  \n\nSurface Area Calculations: \n               Sphere==>5\n                 Cube==>6\n    Rectangular Prism==>7\n                 Cone==>8'
         PT='\033[1;31;40m  \n\nPythagorean Theorem==>9\n'
         TotalInstructions=VC+SAC+PT+Ins
         choice=input(TotalInstructions)
